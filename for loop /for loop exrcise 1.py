42dbdf4f45a38ef3d20f926263ef9eccc31acdc2
zamirowska <25JuliaZamirowska.acc@lwetb.ie>
2026-03-25 15:31:40 +0000

    Rename for loop exrcise 1.py to for loop /for loop exrcise 1.py

---
+++ for loop /for loop exrcise 1.py
@@ -0,0 +1,3 @@
+for count in range (1,31):
+    print (count *'$')
+#prints the output however many times it says in the range 
--- for loop exrcise 1.py
+++
@@ -1,3 +0,0 @@
-for count in range (1,31):
-    print (count *'$')
-#prints the output however many times it says in the range 
