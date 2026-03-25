17e7a7a65bd0291006cc278ea1d4b5c2a442fdb6
zamirowska <25JuliaZamirowska.acc@lwetb.ie>
2026-03-25 15:29:42 +0000

    Add files via upload

---
+++ pythagorean triple 25th.py
@@ -0,0 +1,15 @@
+a_range=input('enter an integar :')
+a_range=int(a_range)
+b_range=input('enter a second integar')
+b_range=int(b_range)
+c_range=input('enter a thrid integar:')
+c_range=int(c_range)
+for a in range(1,a_range):
+    for b in range (1,b_range):
+        for c in range (1,c_range):
+            a_squ= a**2
+            b_squ= b**2
+            c_squ=c**2
+            if( 1,a_range+1,b_range == 1,c_range):
+             print('you have entered a pythagorean triple',a,b,c)
+            
